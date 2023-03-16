dccbc656cb04ac077ce4fc33bbe688071ea99256
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-03-16 14:33:14 +0100

    first commit

---
+++ src/Game.js
@@ -0,0 +1,17 @@
+import TileMap from "./TileMap.js";
+
+const tileSize = 32;
+const velocity = 2;
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const tileMap = new TileMap(tileSize);
+const pacman = tileMap.getPacman(velocity);
+
+function gameloop() {
+  tileMap.draw(ctx);
+  pacman.draw(ctx);
+}
+
+tileMap.setCanvasSize(canvas);
+setInterval(gameloop, 1000 / 75);
